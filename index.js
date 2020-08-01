527345f9c57cb043a27c826b88bdb00c087cc9cb
Anshul Garg <anshulgarg.garg@gmail.com>
2020-08-01 18:48:54 +0530

    adds morgan

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const logger = require('./middlewares/logger');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -14,7 +14,7 @@ mongoose.connect('mongodb://localhost/test', {
   .catch(err => console.log(err));
 
 app.set('port', process.env.PORT || 3000);
-app.use(logger);
+app.use(morgan('dev'));
 app.use(express.json()); //Used to parse JSON bodies
 
 // routes
--- middlewares/logger.js
+++ middlewares/logger.js
@@ -5,3 +5,8 @@ const logger = (req, res, next) => {
 };
 
 module.exports = logger;
+
+// Put below 2 lines in index.json to use this logger:
+
+// const logger = require('./middlewares/logger');
+// app.use(logger);
